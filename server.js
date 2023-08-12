e35185a4038d3c1555b97c8a113e13c363a96514
gilles20393 <gilles1996@outlook.com>
2023-08-12 02:58:24 +0200

    fix publishToCentral

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const addonInterface = require("./addon")
 serveHTTP(addonInterface, { port: process.env.PORT || 60650 })
 
 // when you've deployed your addon, un-comment this line
-publishToCentral("http://3d4f6ecdd115-live-iptv.baby-beamup.club")
+publishToCentral("https://live-iptv.onrender.com/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
